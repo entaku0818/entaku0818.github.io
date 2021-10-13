914d27410f1a298581fd15ca1379c1529f31197d
遠藤拓弥 <entaku19890818@gmail.com>
2021-10-13 18:05:00 +0900

    fortawesomeの追加 内容の修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,154 @@
 import Head from 'next/head'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter,faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export const Home = (): JSX.Element => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <div className="container">
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+        <Head>
+            <title>Portfoilo - entaku</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <header>
+            <h1>
+                <a className="header-title" href="/">ENTAKU</a>
+            </h1>
+            <nav className="pc-nav">
+                <a href="/">home</a>
+            </nav>
+        </header>
+        <main>
 
-      <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
+            <h1 className="title">
+                entaku
+            </h1>
 
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest')
-        }}
-      >
-        Test Button
-      </button>
+            <p className="description">
+                Mobile App Developer
+            </p>
+            <h2>☎️ Contact information</h2>
+            <p>
+                <a
+                    href="https://twitter.com/entaku_0818"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                 <FontAwesomeIcon size="lg" icon={faTwitter} />
+                </a>
+                <a
+                    href="https://twitter.com/entaku_0818"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FontAwesomeIcon size="lg" icon={faGithub} />
+                </a>
+            </p>
+            <h2>👩🏻‍💻Work</h2>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <h3>CBCloud　（2019/05〜）</h3>
+            <a
+                href="https://cb-cloud.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                https://cb-cloud.com/
+            </a>
+            <p>
+                フリーランスドライバーと荷主を即時につなぐ配送マッチングプラットフォームを提供する。
+                最初はモバイルエンジニアとして参画し、webフロントやサーバーサイドなども経験しながらPMとして、開発施策の策定や施策によっては機能開発まで実施する
+            </p>
+            <ul>
+                <li>
+                    一般貨物運送事業者向けのSaasであるSmaRyuTruck開発
+                </li>
+                <li>
+                    一般貨物向け配送依頼画面のリリース  2019/11
+                </li>
+                <li>
+                    買い物アプリリリース - 2020/4 〜 2020/5
+                </li>
+                <li>
+                    PickGo Business/Partner開発 - 2020/10-11〜
+                </li>
+                <li>
+                    JAL航空便連携 2021/3
+                </li>
+                <li>
+                    Flutterリプレイス 2021/3
+                </li>
+            </ul>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+            <h3>LifeSports　（2018/03〜2019/04）</h3>
+            <a
+                href="https://lifesports.jp"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                https://lifesports.jp
+            </a>
+            <p>
+                仕事と自宅の往復をしてた辛い時期に、個人フットサルという、コートでそれぞれが集まってフットサルをするというイベントを見つけ通っていました。
+                そのフットサルの集まりがとても居心地がよく、その経験からそんな場所をもっと増やしていきたいのでスポーツ事業にエンジニアとして関わってみたいと考え、ライフスポーツへ転職。
+                主にユーザー数を増やすような既存機能の修正やコミュニティ機能の設計実装を実施した。
+            </p>
 
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+            <h3>株式会社エーピーコミュニケーションズ　（2013/09〜2018/02）</h3>
+            <a
+                href="https://www.ap-com.co.jp"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                https://www.ap-com.co.jp
+            </a>
+            <h4>ネットワークエンジニア （2013/09〜2016/02）</h4>
+            <p>
+                大手NW事業会社 　NWサービスの認証システムの開発と新たなシステムへのリプレイス作業を実施。
+                主に新しいNWサービスを導入する際のシステム設計/開発/検証/導入までのNW/サーバー設計をメインで担当。
+            </p>
+            <h4>アプリケーションエンジニア （2016/03〜2018/02）</h4>
+            <p>
+                主に教育事業会社や娯楽事業のサービス保守システムリプレイスなどを実施
+                CakePHP/Laravel/Spring/MySQL
+            </p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
+            <h3>日本ユニアンドソニック（2010/04〜2013/08）</h3>
+            <a
+                href="https://www.sonic-square.co.jp/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                https://www.sonic-square.co.jp/
+            </a>
+            <p>レンタル事業会社へ派遣され、システム開発を従事</p>
 
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} />
-      </a>
-    </footer>
+            <h2>🗣 Languages</h2>
+            <p>日本語</p>
+            <h2>📚Education</h2>
+            <p>
+                <a
+                    href="https://bbt.ac/"
+                    rel="noopener noreferrer"
+                >
+                    ビジネスブレイクスルー大学
+                </a>
+                (2018年卒業)
+            </p>
+            <p>
+                日本工学院八王子専門学校 (2010年卒業)
+            </p>
 
-    <style jsx>{`
+        </main>
+
+        <footer>
+            Powered by entaku
+        </footer>
+
+        <style jsx>{`
+      .header-title{
+        font-family: Arial, cursive;
+        font-size: 36px;
+        height: min-content;
+      }
+
       .container {
         min-height: 100vh;
         padding: 0 0.5rem;
@@ -80,7 +164,7 @@ export const Home = (): JSX.Element => (
         display: flex;
         flex-direction: column;
         justify-content: center;
-        align-items: center;
+        margin: 10%;
       }
 
       footer {
@@ -112,10 +196,10 @@ export const Home = (): JSX.Element => (
         text-decoration: none;
       }
 
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
+      .a:hover,
+      .a:focus,
+      .a:active {
+        text-dec  oration: underline;
       }
 
       .title {
@@ -189,9 +273,27 @@ export const Home = (): JSX.Element => (
           flex-direction: column;
         }
       }
+      * {
+     box-sizing: border-box;
+    }
+
+    header {
+       padding: 10px 4%;
+       position: fixed;
+       top: 0;
+       width: 100%;
+       display: flex;
+       align-items: center;
+       background: #fff;
+    }
+
+    nav {
+       margin: 0 0 0 auto;
+    }
+
     `}</style>
 
-    <style jsx global>{`
+        <style jsx global>{`
       html,
       body {
         padding: 0;
@@ -204,7 +306,7 @@ export const Home = (): JSX.Element => (
         box-sizing: border-box;
       }
     `}</style>
-  </div>
+    </div>
 )
 
 export default Home
