ffbc26e27e742a23cde79ab2ef77a2e24124f0c1
entaku0818 <entaku0818@users.noreply.github.com>
2021-10-13 09:47:33 +0000

    deploy: 3ab05e3fd82f395049e94d2ac98dee1f1ea486b1

--- _next/static/ckYq5--i6AWLoiEpKysVx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3bc7a5a0f465d90c8c39.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
